b499382103496146caa3d5e0f0af75bf77a77fec
Danijel Lakic <danijellakic@yahoo.com>
2022-09-28 15:43:07 +0200

    Initialize project using Create React App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import SearchMovies from "./searchMovies";
+import Header from "./header";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <video loop autoPlay muted id="bg--video">
+        <source src={require ('../src/video/movie.mp4')} type="video/mp4" />
+      </video>
+      <Header/>
+      <SearchMovies/>
     </div>
   );
 }
---
+++ src/header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function header() {
+  return (
+    <div>
+      <h1 className="title">Start looking for your favorite movie</h1>
+    </div>
+  )
+}
